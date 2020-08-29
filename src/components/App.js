cc45ece18c6dd38ca7587025b025c6041f24cb36
Emily <emseibert@gmail.com>
2020-08-29 14:50:01 -0600

    send preview - bug on line breaks

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
 
 import Canvas from "./Canvas";
 
 const App = () => {
+  const channel = useMemo(() => new BroadcastChannel("graffiti"), []);
+  const drawPath = useSelector((state) => state.draw);
+
+  const onClick = (e) => {
+    e.preventDefault();
+    // open a new window for /preview
+    const newWindow = window.open(`${window.location.origin}/preview.html`);
+    newWindow.addEventListener("load", () => {
+      // send store of draw
+      channel.postMessage(drawPath);
+    });
+  };
+
   return (
     <>
       <h1 className="text-4xl text-white bg-black">Hello</h1>
-      <a href="./preview.html" target="_blank">
-        Link to another page
+      <a href="./preview.html" onClick={onClick}>
+        See preview
       </a>
       <Canvas />
     </>
--- src/preview.js
+++ src/preview.js
@@ -6,17 +6,24 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
 const draw = (channelCoords) => {
+  if (!channelCoords.x) {
+    coord.x = null;
+    coord.y = null;
+    return;
+  }
+
   // original logic from: https://www.geeksforgeeks.org/how-to-draw-with-mouse-in-html-5-canvas/
   ctx.beginPath();
-
   ctx.lineWidth = 5;
   ctx.lineCap = "round";
   ctx.strokeStyle = "green";
 
-  // if null, then line break. use same coords for both before & after of stroke.
+  // if previous coord.x is null, then we've registered line break.
+  // use same coords for both before & after of stroke to begin a new line
   if (coord.x) {
     ctx.moveTo(coord.x, coord.y);
   } else {
+    console.log("line break!", channelCoords);
     ctx.moveTo(channelCoords.x, channelCoords.y);
   }
 
@@ -30,9 +37,8 @@ const draw = (channelCoords) => {
 
 channel.addEventListener("message", (event) => {
   // if data at x coord is null, then mouseup has fired. reset coords.
-  if (!event.data.x) {
-    coord.x = null;
-    coord.y = null;
+  if (Array.isArray(event.data)) {
+    event.data.forEach((pos) => draw(pos));
   } else {
     draw(event.data);
   }
