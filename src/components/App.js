5f97e4427a319fc14d1260f78d4f2236b09f36c4
Emily <emseibert@gmail.com>
2020-08-29 11:58:31 -0600

    setup cross page communication for broadcast

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useMemo, useEffect } from "react";
 
 import Canvas from "./Canvas";
 
 const App = () => {
+  const channel = useMemo(() => new BroadcastChannel("graffiti"), []);
+
+  const sendTestMsg = () => {
+    channel.postMessage("testing!");
+  };
+
   return (
     <>
       <h1 className="text-4xl text-white bg-black">Hello</h1>
+      <a href="./preview.html" target="_blank">
+        Link to another page
+      </a>
+      <button onClick={sendTestMsg}>Testing channel broadcasting!</button>
       <Canvas />
     </>
   );
--- src/preview.js
+++ src/preview.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", () => {
   console.log("successfully loaded preview JS page");
 });
+
+const channel = new BroadcastChannel("graffiti");
+
+channel.addEventListener("message", (event) => {
+  console.log("event.data", event.data);
+});
