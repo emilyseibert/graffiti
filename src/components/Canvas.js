937ec73dad009aa6a6b8bd236ecf8f3d44db6bf8
Emily <emseibert@gmail.com>
2020-08-29 12:12:16 -0600

    drawing state

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,8 +1,10 @@
-import React, { useMemo, useEffect } from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleBroadcast } from "../actions";
 
 const Canvas = () => {
+  const [isDrawing, setIsDrawing] = useState(false);
+
   const dispatch = useDispatch();
   const broadcasting = useSelector((state) => state.broadcast);
   const toggleBroadcasting = () => {
@@ -15,12 +17,29 @@ const Canvas = () => {
     channel.postMessage(`broadcast is ${broadcasting ? "on" : "off"}`);
   }, [broadcasting]);
 
+  useEffect(() => {
+    const handleMouse = (e) => {
+      const isStartingToDraw = e.type === "mousedown";
+      setIsDrawing(isStartingToDraw);
+    };
+
+    document.addEventListener("mousedown", handleMouse);
+    document.addEventListener("mouseup", handleMouse);
+
+    return () => {
+      document.removeEventListener("mousedown", handleMouse);
+      document.removeEventListener("mouseup", handleMouse);
+    };
+  }, [isDrawing]);
+
   return (
     <div>
       <h1>Canvas Component</h1>
       <div>Broadcast is {broadcasting ? "on" : "off"}</div>
+      <div> {isDrawing ? "drawing!" : "not drawing"}</div>
 
       <button onClick={toggleBroadcasting}>Toggle broadcasting</button>
+      <canvas></canvas>
     </div>
   );
 };
